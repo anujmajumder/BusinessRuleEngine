e921819801c9d6a75feda407b3fa62b3b539ce8b
Anuj <anujmajumder.am@gmail.com>
2020-09-16 08:35:56 +0000

    Creating the OrdersDao

--- Development/src/main/java/com/anuj/maven/orders/Order.java
+++ Development/src/main/java/com/anuj/maven/orders/Order.java
@@ -4,5 +4,40 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Order {
+	
+	
+	String entity;
+	String email;
+	double payment;
+	
+	public Order() {
+		super();
+	}
+	
+	public Order(String entity, String email, Double payment) {
+		
+		this.entity=entity;
+		this.email=email;
+		this.payment=payment;
+	}
+	public String getEntity() {
+		return entity;
+	}
+	public void setEntity(String entity) {
+		this.entity = entity;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public double getPayment() {
+		return payment;
+	}
+	public void setPayment(double payment) {
+		this.payment = payment;
+	}
+	
 
 }
---
+++ Development/src/main/java/com/anuj/maven/orders/dao/OrdersDao.java
@@ -0,0 +1,30 @@
+package com.anuj.maven.orders.dao;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import com.anuj.maven.orders.Order;
+
+@Component
+public class OrdersDao {
+	
+private NamedParameterJdbcTemplate jdbc;
+	
+	
+	@Autowired
+	public void setDataSource(DataSource jdbc) {
+		this.jdbc = new NamedParameterJdbcTemplate(jdbc);
+	}
+	
+	public  boolean create(Order order) {
+		
+		BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(order);
+	
+		return jdbc.update("INSERT into order (entity,email,payment) VALUES (:entity,:email,:payment)", params) == 1;
+	}
+
+}
